0a29550ccf56d02371c13f2834c68d345c0229a9
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-14 10:42:55 -0300

    Initial Commit

---
+++ InfoTelefoniaTests/InfoTelefoniaTests.h
@@ -0,0 +1,13 @@
+//
+//  InfoTelefoniaTests.h
+//  InfoTelefoniaTests
+//
+//  Created by Rafael Brigagão Paulino on 14/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface InfoTelefoniaTests : SenTestCase
+
+@end
---
+++ InfoTelefoniaTests/InfoTelefoniaTests.m
@@ -0,0 +1,32 @@
+//
+//  InfoTelefoniaTests.m
+//  InfoTelefoniaTests
+//
+//  Created by Rafael Brigagão Paulino on 14/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "InfoTelefoniaTests.h"
+
+@implementation InfoTelefoniaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in InfoTelefoniaTests");
+}
+
+@end
